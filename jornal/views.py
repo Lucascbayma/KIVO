613ab3a57befbbf8143774c872d509ec5a3e0d0b
Lucascbayma <Lucasbayma31@gmail.com>
2025-10-13 17:39:24 -0300

    Função artigo implentada nas views.py

--- jornal/views.py
+++ jornal/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.views.generic import DetailView
+from .models import Artigo
+
+class ArtigoDetailView(DetailView):
+    model = Artigo
+    template_name = 'templates/artigo.html'
+    context_object_name = 'artigo'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        artigo_atual = self.object 
+
+        context['recomendacoes'] = artigo_atual.artigos_relacionados(limite=3) 
+        
+        return context
+    
