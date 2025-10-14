f26b22d4ff0f12b6ed13dc7afe76b7f6f673876c
Eros Amancio Nascimento <ean@cesar.school>
2025-10-14 10:36:44 -0300

    adicionei a pagina de leitura da noticia.

--- jornal/urls.py
+++ jornal/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ArtigoDetailView
+from .views import ArtigoDetailView, detalhe_noticia_estatica # Importe a view estática
 
 urlpatterns = [
-    path('artigo/<int:pk>/',ArtigoDetailView.as_view(),name='detalhe_artigo'),
+    path('noticia-estatica/', detalhe_noticia_estatica, name='detalhe_noticia_estatica'),
+    path('artigo/<int:pk>/', ArtigoDetailView.as_view(), name='detalhe_artigo'),
 ]
--- jornal/views.py
+++ jornal/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.views.generic import DetailView
-from .models import Artigo
+
+
+from .models import Artigo 
+
 
 class ArtigoDetailView(DetailView):
     model = Artigo
-    template_name = 'templates/artigo.html'
-    context_object_name = 'artigo'
+    template_name = 'ler_noticia.html' 
+    context_object_name = 'artigo' 
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        
+
+        context = super().get_context_data(**kwargs)    
         artigo_atual = self.object 
 
-        context['recomendacoes'] = artigo_atual.artigos_relacionados(limite=3) 
+        try:
+
+             context['recomendacoes'] = artigo_atual.artigos_relacionados(limite=3) 
+        except AttributeError:
+
+             context['recomendacoes'] = []
         
         return context
-    
+
+def detalhe_noticia_estatica(request):
+    """Renderiza o template 'ler_noticia.html' com dados estáticos."""
+
+    return render(request, 'ler_noticia.html', {})
