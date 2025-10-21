96e4cc7d24c2cc244be12ed8ead0d8619f99fbbe
Lucascbayma <Lucasbayma31@gmail.com>
2025-10-21 10:14:16 -0300

    Ajustado erro de sintaxe

--- jornal/urls.py
+++ jornal/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('noticia-estatica/', detalhe_noticia_estatica, name='detalhe_noticia_estatica'),
-    path('artigo/<int:pk>/', ArtigoDetailView.as_view(), name='detalhe_artigo'),
+    path('artigo/<int:pk>/', ArtigoDetailView.as_view(), name='artigo_detalhe'),
     path('adicionar/', views.adicionar_artigo, name='adicionar_artigo'),
     path('', views.home, name='home'),
 ]
